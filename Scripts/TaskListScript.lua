79f839336009bc56c35b0608f01be82b29c9f141
Abdallah Sher <abdallah_sher@outlook.com>
2023-10-31 13:17:16 -0400

    0.2 Update
    see changelog

--- Scripts/TaskListScript.lua
+++ Scripts/TaskListScript.lua
@@ -1,14 +1,14 @@
 function Initialize()
     taskCount = 0
     taskPath = SKIN:MakePathAbsolute('\\@Resources\\taskPersistent.txt')
-    taskFile = io.open(taskPath, "a+")
-    io.output(taskFile)
+    taskFile = io.open(taskPath, "r")
     taskList = {}
+    taskString = ""
     for line in taskFile:lines() do
         taskList[#taskList+1] = {0, line};
         taskCount = taskCount + 1
     end
-    taskString = ""
+    io.close(taskFile)
     needRefresh = 1
 end
 
@@ -28,28 +28,32 @@ function concatList()
 end
 
 function addTask()
+    taskFile = io.open(taskPath, "a+")
+    io.output(taskFile)
     task = SKIN:GetVariable('NewTask')
     print(task)
     taskList[#taskList+1] = {0, task}
     needRefresh = 1
     io.write('\n'..task)
+    io.close(taskFile)
 end
 
 function removeTask(index)
     table.remove(taskList, index)
     print(index)
-    io.close()
     os.remove(taskPath)
     taskFile = io.open(taskPath, "a+")
     io.output(taskFile)
     for i = 1, #taskList do
         io.write(taskList[i][2]..'\n')
     end
+    io.close(taskFile)
     needRefresh = 1
 end
 
 function detLine()
     mouseX = SKIN:GetVariable('$MouseX$')
     mouseY = SKIN:GetVariable('MouseYPos')
-    removeTask(math.ceil(mouseY/40))
+    removeTask(math.ceil(mouseY/26))
+    print("Y: "..mouseY)
 end
